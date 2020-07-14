0dfc0db729c58d06dca6869f20887567a6093691
Krishna Mohan Gollapalli <gkmohan00@gmail.com>
2020-07-14 18:47:53 -0400

    Add files via upload

---
+++ Bayes_rule.py
@@ -0,0 +1,217 @@
+# visualizing Naive Bayes
+# Confident ellipse a tool for representing the naive bayes model visually
+
+'''
+Confidence Ellipse:
+It is a better way than plotting the points over a cartesian plane because,
+with big datasets, the points can overlap badly and hide the real distribution of the data.
+Confidence ellipses summarize the information of the dataset with only four parameters:
+    Center: It is the numerical mean of the attributes
+    Height and width: Related with the variance of each attribute. The user must specify the desired amount of standard deviations used to plot the ellipse.
+    Angle: Related with the covariance among attributes.
+'''
+
+from util_s import process_tweet, lookup
+import pdb
+from nltk.corpus import stopwords, twitter_samples
+import numpy as np
+
+import nltk
+import string
+from nltk.tokenize import TweetTokenizer
+from os import getcwd
+import random
+
+nltk.download('stopwords')
+nltk.download('twitter_samples')
+filePath = f"{getcwd()}/../tmp2/"
+nltk.data.path.append(filePath)
+
+all_positive_tweets=twitter_samples.strings('positive_tweets.json')
+all_negative_tweets=twitter_samples.strings('negative_tweets.json')
+# split the data into two pieces, one for training and one for testing (validation set)
+test_pos = all_positive_tweets[4000:]
+train_pos = all_positive_tweets[:4000]
+test_neg = all_negative_tweets[4000:]
+train_neg = all_negative_tweets[:4000]
+
+train_x = train_pos + train_neg
+test_x = test_pos + test_neg
+
+# avoid assumptions about the length of all_positive_tweets
+train_y = np.append(np.ones(len(train_pos)), np.zeros(len(train_neg)))
+test_y = np.append(np.ones(len(test_pos)), np.zeros(len(test_neg)))
+
+custom_tweet = train_x[random.randint(0, 8000)]
+#print(custom_tweet)
+# print cleaned tweet
+#print(process_tweet(custom_tweet))
+
+def count_tweets(result, tweets, ys):
+    for y, tweet in zip(ys, tweets):
+        for word in process_tweet(tweet):
+            # define the key, which is the word and label tuple
+            pair = (word,y)
+
+            # if the key exists in the dictionary, increment the count
+            if pair in result:
+                result[pair] += 1
+
+            # else, if the key is new, add it to the dictionary and set the count to 1
+            else:
+                result[pair] = 1
+    return result
+
+freqs = count_tweets({}, train_x, train_y)
+
+def train_naive_bayes(freqs, train_x, train_y):
+
+    loglikelihood = {}
+    logprior = 0
+
+    # calculate V, the number of unique words in the vocabulary
+    vocab = set([pair[0] for pair in freqs.keys()])
+    V = len(vocab)
+
+    # calculate N_pos and N_neg
+    N_pos = N_neg = 0
+    for pair in freqs.keys():
+        # if the label is positive (greater than zero)
+        if pair[1] > 0:
+
+            # Increment the number of positive words by the count for this (word, label) pair
+            N_pos += freqs[pair]
+
+        # else, the label is negative
+        else:
+
+            # increment the number of negative words by the count for this (word,label) pair
+            N_neg += freqs[pair]
+
+    # Calculate D, the number of documents
+    D = len(train_y)
+
+    # Calculate D_pos, the number of positive documents (*hint: use sum(<np_array>))
+    D_pos = (len(list(filter(lambda x: x > 0, train_y))))
+
+    # Calculate D_neg, the number of negative documents
+    D_neg = (len(list(filter(lambda x: x <= 0, train_y))))
+
+    # Calculate logprior
+    logprior = np.log(D_pos)-np.log(D_neg)
+
+    # For each word in the vocabulary...
+    for word in vocab:
+        # get the positive and negative frequency of the word
+        freq_pos = lookup(freqs,word,1)
+        freq_neg = lookup(freqs,word,0)
+
+        # calculate the probability that each word is positive, and negative
+        p_w_pos = (freq_pos + 1) / (N_pos + V)
+        p_w_neg = (freq_neg + 1) / (N_neg + V)
+
+        # calculate the log likelihood of the word
+        loglikelihood[word] = np.log(p_w_pos/p_w_neg)
+
+    return logprior, loglikelihood
+
+logprior, loglikelihood = train_naive_bayes(freqs, train_x, train_y)
+print(logprior)
+print(len(loglikelihood))
+
+def naive_bayes_predict(tweet, logprior, loglikelihood):
+
+    # process the tweet to get a list of words
+    word_l = process_tweet(tweet)
+
+    # initialize probability to zero
+    p = 0
+
+    # add the logprior
+    p += logprior
+
+    for word in word_l:
+
+        # check if the word exists in the loglikelihood dictionary
+        if word in loglikelihood:
+            # add the log likelihood of that word to the probability
+            p += loglikelihood[word]
+
+    return p
+
+def test_naive_bayes(test_x, test_y, logprior, loglikelihood):
+
+    accuracy = 0  # return this properly
+
+    y_hats = []
+    for tweet in test_x:
+        # if the prediction is > 0
+        if naive_bayes_predict(tweet, logprior, loglikelihood) > 0:
+            # the predicted class is 1
+            y_hat_i = 1
+        else:
+            # otherwise the predicted class is 0
+            y_hat_i = 0
+
+        # append the predicted class to the list y_hats
+        y_hats.append(y_hat_i)
+
+    # error is the average of the absolute values of the differences between y_hats and test_y
+    error = np.mean(abs(y_hats-test_y))
+
+    # Accuracy is 1 minus the error
+    accuracy = 1-error
+
+    return accuracy
+def get_ratio(freqs, word):
+
+    pos_neg_ratio = {'positive': 0, 'negative': 0, 'ratio': 0.0}
+    # use lookup() to find positive counts for the word (denoted by the integer 1)
+    pos_neg_ratio['positive'] = lookup(freqs,word,1)
+
+    # use lookup() to find negative counts for the word (denoted by integer 0)
+    pos_neg_ratio['negative'] = lookup(freqs,word,0)
+
+    # calculate the ratio of positive to negative counts for the word
+    pos_neg_ratio['ratio'] = (pos_neg_ratio['positive'] + 1)/(pos_neg_ratio['negative'] + 1)
+    return pos_neg_ratio
+
+def get_words_by_threshold(freqs, label, threshold):
+
+    word_list = {}
+
+    for key in freqs.keys():
+        word, _ = key
+
+        # get the positive/negative ratio for a word
+        pos_neg_ratio = get_ratio(freqs, word)
+
+        # if the label is 1 and the ratio is greater than or equal to the threshold...
+        if label == 1 and pos_neg_ratio['ratio'] >= threshold :
+
+            # Add the pos_neg_ratio to the dictionary
+            word_list[word] = pos_neg_ratio
+
+        # If the label is 0 and the pos_neg_ratio is less than or equal to the threshold...
+        elif label == 0 and pos_neg_ratio['ratio'] <= threshold:
+
+            # Add the pos_neg_ratio to the dictionary
+            word_list[word] = pos_neg_ratio
+
+        # otherwise, do not include this word in the list (do nothing)
+
+    return word_list
+
+get_words_by_threshold(freqs, label=0, threshold=0.05)
+
+print('Truth Predicted Tweet')
+for x, y in zip(test_x, test_y):
+    y_hat = naive_bayes_predict(x, logprior, loglikelihood)
+    if y != (np.sign(y_hat) > 0):
+        print('%d\t%0.2f\t%s' % (y, np.sign(y_hat) > 0, ' '.join(
+            process_tweet(x)).encode('ascii', 'ignore')))
+
+my_tweet = 'I am happy that i did not go, he is not happy that he is gone)'
+
+p = naive_bayes_predict(my_tweet, logprior, loglikelihood)
+print(p)
